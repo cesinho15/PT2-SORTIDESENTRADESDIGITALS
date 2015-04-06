e21a4179e3d5cabf7a1ec654eb28231b2521dc04
cesinho15 <cesar_gato.99@hotmail.com>
2015-04-06 19:56:29 +0200

    1.3ESQUERRA_DRETA

--- _1.3ESQUERRA_DRETA/_1.3ESQUERRA_DRETA.ino
+++ _1.3ESQUERRA_DRETA/_1.3ESQUERRA_DRETA.ino
@@ -101,4 +101,73 @@ void loop() {
   digitalWrite(6, HIGH); 
   
   delay(100);     
+  
+    digitalWrite(13, LOW); 
+  digitalWrite(12, LOW); 
+  digitalWrite(11, LOW); 
+  digitalWrite(10, LOW); 
+  digitalWrite(9, LOW); 
+  digitalWrite(8, LOW); 
+  digitalWrite(7, HIGH); 
+  digitalWrite(6, LOW); 
+  
+  delay(100);   
+  
+  digitalWrite(13, LOW);  
+  digitalWrite(12, LOW); 
+  digitalWrite(11, LOW); 
+  digitalWrite(10, LOW); 
+  digitalWrite(9, LOW); 
+  digitalWrite(8, HIGH); 
+  digitalWrite(7, LOW); 
+  digitalWrite(6, LOW); 
+  
+  delay(100);  
+  
+  digitalWrite(13, LOW);  
+  digitalWrite(12, LOW); 
+  digitalWrite(11, LOW); 
+  digitalWrite(10, LOW); 
+  digitalWrite(9, HIGH); 
+  digitalWrite(8, LOW); 
+  digitalWrite(7, LOW); 
+  digitalWrite(6, LOW); 
+  
+  delay(100);  
+  
+  digitalWrite(13, LOW);  
+  digitalWrite(12, LOW); 
+  digitalWrite(11, LOW); 
+  digitalWrite(10, HIGH); 
+  digitalWrite(9, LOW); 
+  digitalWrite(8, LOW); 
+  digitalWrite(7, LOW); 
+  digitalWrite(6, LOW); 
+  
+  delay(100);  
+
+  digitalWrite(13, LOW);  
+  digitalWrite(12, LOW); 
+  digitalWrite(11, HIGH); 
+  digitalWrite(10, LOW); 
+  digitalWrite(9, LOW); 
+  digitalWrite(8, LOW); 
+  digitalWrite(7, LOW); 
+  digitalWrite(6, LOW); 
+  
+  delay(100);   
+  
+  digitalWrite(13, LOW);  
+  digitalWrite(12, HIGH); 
+  digitalWrite(11, LOW); 
+  digitalWrite(10, LOW); 
+  digitalWrite(9, LOW); 
+  digitalWrite(8, LOW); 
+  digitalWrite(7, LOW); 
+  digitalWrite(6, LOW); 
+  
+  delay(100);     
+  
+  
+ 
 }
