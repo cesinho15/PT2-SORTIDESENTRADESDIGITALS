cdf7c503cf227c3339d7daa3f243e99fb786c2ce
cesinho15 <cesar_gato.99@hotmail.com>
2015-04-06 19:52:04 +0200

    1.3CENTRE_EXTREMS

---
+++ _1.3CENTRE_EXTREMS/_1.3CENTRE_EXTREMS.ino
@@ -0,0 +1,62 @@
+
+void setup() {
+  
+  pinMode(13, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(8, OUTPUT);
+  pinMode(7, OUTPUT);
+  pinMode(6, OUTPUT);
+
+}
+
+
+void loop() {
+  digitalWrite(13, LOW); 
+  digitalWrite(12, LOW); 
+  digitalWrite(11, LOW); 
+  digitalWrite(10, HIGH); 
+  digitalWrite(9, HIGH); 
+  digitalWrite(8, LOW); 
+  digitalWrite(7, LOW); 
+  digitalWrite(6, LOW); 
+  
+  delay(200);   
+  
+  digitalWrite(13, LOW);  
+  digitalWrite(12, LOW); 
+  digitalWrite(11, HIGH); 
+  digitalWrite(10, LOW); 
+  digitalWrite(9, LOW); 
+  digitalWrite(8, HIGH); 
+  digitalWrite(7, LOW); 
+  digitalWrite(6, LOW); 
+  
+  delay(200);  
+  
+  digitalWrite(13, LOW);  
+  digitalWrite(12, HIGH); 
+  digitalWrite(11, LOW); 
+  digitalWrite(10, LOW); 
+  digitalWrite(9, LOW); 
+  digitalWrite(8, LOW); 
+  digitalWrite(7, HIGH); 
+  digitalWrite(6, LOW); 
+  
+  delay(200);  
+  
+  digitalWrite(13, HIGH);  
+  digitalWrite(12, LOW); 
+  digitalWrite(11, LOW); 
+  digitalWrite(10, LOW); 
+  digitalWrite(9, LOW); 
+  digitalWrite(8, LOW); 
+  digitalWrite(7, LOW); 
+  digitalWrite(6, HIGH); 
+  
+  delay(200);  
+
+   
+}
